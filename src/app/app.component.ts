372606df87c8d3edcddcfd5de131120479229b96
zohra-20 <zohra.moutaouakkil.j@gmail.com>
2021-06-08 16:14:50 +0200

    16:14

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'my-app';
+  maProp = 'yellow';
+  show = true;
+  info = {
+    nom: 'Alex',
+    email: 'alex@',
+    tel: '06xxxxx',
+  };
+
+  getName() {
+    return this.info.nom;
+  }
+
+  changeStatus() {
+    this.show = ! this.show;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TestComponentComponent } from './test-component/test-component.component';
+import { CategorieComponent } from './categorie/categorie.component';
+import { ProduitComponent } from './produit/produit.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponentComponent,
+    CategorieComponent,
+    ProduitComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
